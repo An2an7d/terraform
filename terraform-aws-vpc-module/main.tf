fd4bea84f2ec1fb65826839d821e95cec102b80d
Anand <venkatanandn@gmail.com>
2023-10-09 20:44:17 +0530

    modified

--- terraform-aws-vpc-module/main.tf
+++ terraform-aws-vpc-module/main.tf
@@ -101,7 +101,8 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.main.id
+    nat_gateway_id = aws_nat_gateway.main.id
+    
   }
 
   tags = merge(
@@ -118,7 +119,7 @@ resource "aws_route_table" "database" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.main.id
+    nat_gateway_id = aws_nat_gateway.main.id
   }
 
   tags = merge(
